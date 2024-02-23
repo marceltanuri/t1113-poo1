7fd45f9966cb1effbda4b1d553c02a8757305092
Marcel Tanuri <marcel.tanuri@gmail.com>
2024-02-22 22:46:03 -0300

    code format

--- src/com/mtanuri/t1113/controller/command/opcoes/OperacoesAtor.java
+++ src/com/mtanuri/t1113/controller/command/opcoes/OperacoesAtor.java
@@ -3,9 +3,6 @@ package com.mtanuri.t1113.controller.command.opcoes;
 import com.mtanuri.t1113.controller.command.Command;
 
 public enum OperacoesAtor {
-
-
-
         INSERIR, LISTAR_TODOS, PESQUISAR_POR_NOME, EXCLUIR, RENOMEAR, ADICIONAR_FILME, REMOVER_FILME
 
 }
--- src/com/mtanuri/t1113/controller/command/opcoes/OperacoesDiretor.java
+++ src/com/mtanuri/t1113/controller/command/opcoes/OperacoesDiretor.java
@@ -1,7 +1,6 @@
 package com.mtanuri.t1113.controller.command.opcoes;
 
 public enum OperacoesDiretor {
-	
 	INSERIR, LISTAR_TODOS, PESQUISAR_POR_NOME, EXCLUIR, RENOMEAR, ADICIONAR_FILME, REMOVER_FILME
 
 }
